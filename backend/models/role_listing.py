3d953b675d85d0d4659a151c3504934077d8f68f
faithangg <89286947+faithangg@users.noreply.github.com>
2023-09-16 15:24:37 +0800

    created role listing model

---
+++ backend/models/role_listing.py
@@ -0,0 +1,19 @@
+from database import db
+
+class RoleListing(db.Model):
+    __tablename__ = 'role_listing'
+
+    listing_id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(20), nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
+    deadline = db.Column(db.Date, nullable=False)
+
+    def json(self):
+        return {
+            'listing_id': self.listing_id,
+            'role_name': self.role_name,
+            'department': self.department,
+            'category': self.category,
+            'deadline': self.deadline.strftime('%Y-%m-%d')  # Format the date as a string if needed
+        }
