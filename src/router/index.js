37b92c611df7ea37cb17bfba6de2ffaa0607ac8b
Pine <leeleetan0808@gmail.com>
2023-09-19 01:15:12 +0800

    created UI for 'create role listing' form

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue";
+import CreateRoleListing from "../views/CreateRoleListing.vue";
+
 
 const routes = [
   {
     path: "/",
     name: "HomePage",
     component: HomePage,
+  },
+  {
+    path: "/CreateRoleListing",
+    name: "CreateRoleListing",
+    component: CreateRoleListing,
   },
 
 ];
---
+++ src/views/CreateRoleListing.vue
@@ -0,0 +1,106 @@
+<template>
+<v-card max-width="948" class="mx-auto" color="grey-lighten-3">
+    <v-layout>    
+        <v-main>
+            <div data-aos="fade-up" data-aos-duration="1000">
+                <v-container fluid>
+                    <form @submit.prevent="submit">
+                        <v-card elevation="2" class="pa-14">
+                            <h1 class="ps-0 pb-8">Create New Role Listing</h1>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Job title* 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-text-field type="text" v-model="email" label="Enter job title" variant="outlined" required></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Description 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-textarea type="text" v-model="email" label="Enter job scope" variant="outlined" required></v-textarea>
+                                </v-col>
+                            </v-row>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Department 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-select type="text" v-model="email" label="Select department" variant="outlined" required :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+                                </v-col>
+                            </v-row>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Category 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-select type="text" v-model="email" label="Select category(s)" variant="outlined" required :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" chips multiple></v-select>
+                                </v-col>
+                            </v-row>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Job Level 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-select type="text" v-model="email" label="Select job level" variant="outlined" required :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+                                </v-col>
+                            </v-row>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Country 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-select type="text" v-model="email" label="Select country" variant="outlined" required :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+                                </v-col>
+                            </v-row>
+                            <v-row dense>
+                                <v-col cols="4">
+                                    <p class="text-h6 font-weight-bold pt-4">
+                                        Skills 
+                                    </p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-select type="text" v-model="email" label="Select skill(s)" variant="outlined" required :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" chips multiple></v-select>
+                                </v-col>
+                            </v-row>
+                            <v-row dense class="mx-16">
+                                <v-btn
+                                    block
+                                    class="mt-8"
+                                    color="teal-lighten-1"
+                                    size="large"
+                                    variant="tonal"
+                                >
+                                    <b>Publish</b>
+                                </v-btn>
+                            </v-row>
+                            
+                        </v-card>
+                    </form>
+                </v-container>
+            </div>            
+        </v-main>    
+    </v-layout>
+</v-card>
+</template>
+
+<script>
+    export default {
+        name: "CreateRoleListing",
+    }
+</script>
+
