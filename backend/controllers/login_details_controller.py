dd59cab77dd20b4ae94381fbe8b002d357cfe1d5
Balveen Kaur <balveenkaur01@gmail.com>
2023-09-18 16:55:42 +0800

    modification in login controller

--- backend/controllers/login_details_controller.py
+++ backend/controllers/login_details_controller.py
@@ -3,21 +3,12 @@ from database import db
 from models.login_details import LoginDetails
 from models.staff import Staff
 from blueprints.staff_blueprint import staff_blueprint
-import rsa
 
 # Handles the logic where HR creates role listing
 @staff_blueprint.route('/login_details', methods=['GET'])
 def login_details():
     
     try:
-        publicKey, privateKey = rsa.newkeys(512)
-        message = "hashed_password_for_john"
-
-        encMessage = rsa.encrypt(message.encode(),publicKey)
-        print(encMessage)
-
-
-
         # Extract data from the request
         data = request.get_json()
         staff_id = data['staff_id']
@@ -28,11 +19,9 @@ def login_details():
         login = LoginDetails.query.filter_by(staff_id=staff_id).first()
         
         if login:
-            db_password_encoded = login.staff_password
-            db_password_decoded = login.staff_password
+            db_password = login.staff_password
 
-            # print(db_password_decoded)
-            if(db_password_decoded != staff_password):
+            if(db_password != staff_password):
                 return jsonify(
                     {
                         "code": 500,
