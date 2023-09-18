a140d9d2df07bc15e94ccb0f9a751daf62481b81
Balveen Kaur <balveenkaur01@gmail.com>
2023-09-18 16:56:43 +0800

    role skill match backend codes

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ from blueprints.staff_blueprint import staff_blueprint
 import controllers.role_creation_controller # This line ensures that your route functions get registered
 import controllers.login_details_controller
 import controllers.role_listing_controller
+import controllers.role_skill_match_controller
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/sbrp'
