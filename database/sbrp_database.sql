a7237e41699a01532c95009331d877cc54050f69
Balveen Kaur <balveenkaur01@gmail.com>
2023-09-19 12:18:27 +0800

    database modifications

--- database/sbrp_database.sql
+++ database/sbrp_database.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `access_rights`(
     access_right integer NOT NULL,
     user_role varchar(20) NOT NULL,
     CONSTRAINT access_rights_pkey PRIMARY KEY (access_right)
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO access_rights (access_right, user_role) VALUES
 (1, 'HR'),
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `staff` (
     access_rights integer NOT NULL,
     CONSTRAINT staff_pkey PRIMARY KEY (staff_id),
     constraint staff_fk1 foreign key (access_rights) references access_rights(access_right) ON DELETE CASCADE
-);  
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO staff (staff_id, staff_fname, staff_lname, dept, email, access_rights) VALUES
 (1, 'John', 'Doe', 'HR', 'john.doe@example.com', 2),
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `role` (
     role_name varchar(20) NOT NULL,
     role_desc longtext NOT NULL,
     CONSTRAINT role_pkey PRIMARY KEY (role_name)
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO role (role_name, role_desc) VALUES
 ('Manager', 'Managerial role'),
@@ -66,7 +66,7 @@ DROP TABLE IF EXISTS `skills`;
 CREATE TABLE IF NOT EXISTS `skills` (
     skill_name varchar(50) NOT NULL,
     CONSTRAINT skills_pkey PRIMARY KEY (skill_name)
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO skills (skill_name) VALUES
 ('Python'),
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `role_skill` (
     CONSTRAINT role_skill_pkey PRIMARY KEY (role_name, skill_name),
     CONSTRAINT role_skill_fk1 FOREIGN KEY (role_name) REFERENCES role(role_name) ON DELETE CASCADE,
     CONSTRAINT role_skill_fk2 FOREIGN KEY (skill_name) REFERENCES skills(skill_name) ON DELETE CASCADE
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO role_skill (role_name, skill_name) VALUES
 ('Manager', 'Python'),
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `staff_skill` (
     CONSTRAINT staff_skill_pkey PRIMARY KEY (staff_id, skill_name),
     CONSTRAINT staff_skill_fk1 FOREIGN KEY (skill_name) REFERENCES skills(skill_name) ON DELETE CASCADE,
     CONSTRAINT staff_skill_fk2 FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE CASCADE
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO staff_skill (staff_id, skill_name) VALUES
 (1, 'Python'),
@@ -108,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `category` (
     category varchar(120) NOT NULL,
     category_desc varchar(100) NOT NULL,
     CONSTRAINT category_pkey PRIMARY KEY (category)
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO category (category, category_desc) VALUES
 ('Administration and Support', 'Provides office support to either an individual or team and is vital for the smooth-running of a business'),
@@ -163,7 +163,7 @@ CREATE TABLE IF NOT EXISTS `login_details` (
     staff_password varchar(255) NOT NULL,
     CONSTRAINT login_details_pkey PRIMARY KEY (staff_id),
     constraint login_details_fk1 foreign key (staff_id) references staff(staff_id) ON DELETE CASCADE
-);
+)  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO login_details (staff_id, staff_password) VALUES
 (1, 'hashed_password_for_john'),
@@ -173,10 +173,10 @@ INSERT INTO login_details (staff_id, staff_password) VALUES
 
 DROP TABLE IF EXISTS `extra`;
 CREATE TABLE IF NOT EXISTS `extra` (
-  `extra_id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`extra_id`)
-  
+    `extra_id` int(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`extra_id`)
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 INSERT INTO `extra` (`extra_id`)VALUES 
 ('1');
 
