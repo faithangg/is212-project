b808793580d5e50742336d44da8ef8db933fd6f7
faithangg <89286947+faithangg@users.noreply.github.com>
2023-09-21 17:34:29 +0800

    logic for duplicate role listings

--- backend/controllers/role_creation_controller.py
+++ backend/controllers/role_creation_controller.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from database import db
 from models.role_listing import RoleListing
 from blueprints.hr_blueprint import hr_blueprint
+from models.role_skill import RoleSkill 
 
 # Handles the logic where HR creates role listing
 @hr_blueprint.route('/create_role_listing', methods=['POST'])
@@ -18,6 +19,21 @@ def create_role_listing():
         # Extract validated fields
         role_name, department, category, deadline = [data[k] for k in required_keys]
 
+        # Query for skills related to the role_name
+        skills_result = RoleSkill.query.filter_by(role_name=role_name).all()
+        skills_list = [skill.skill_name for skill in skills_result] 
+
+        # Check for duplicate role listing
+        existing_role = RoleListing.query.filter_by(
+            role_name=role_name,
+            department=department,
+            category=category,
+            deadline=deadline,
+        ).first()
+
+        if existing_role:
+            return jsonify({"error": "Duplicate role listing"}), 400
+
         # Create a new role listing object
         new_role_listing = RoleListing(
             role_name=role_name,
@@ -33,8 +49,10 @@ def create_role_listing():
         return jsonify(
             {
                 "code": 201,
-                "data": new_role_listing.json()
+                "data": new_role_listing.json(),
+                "skills_required": skills_list  # Include the list of skills in the response
             }  
+            
         ), 201
     except Exception as e:
         db.session.rollback()
