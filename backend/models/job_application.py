9c8869f62ab399ad3ef74eca3cb582d98d5ffbb0
faithangg <89286947+faithangg@users.noreply.github.com>
2023-09-22 23:18:39 +0800

    changed job application model

--- backend/models/job_application.py
+++ backend/models/job_application.py
@@ -5,18 +5,19 @@ class JobApplication(db.Model):
 
     application_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     staff_id = db.Column(db.Integer, nullable=False)
-    role_name = db.Column(db.String(20), nullable=False)
+    listing_id = db.Column(db.Integer, db.ForeignKey('role_listing.listing_id'), nullable=False)
     application_date = db.Column(db.Date, nullable=False)
     
-    def __init__(self, staff_id, role_name, application_date):
+    
+    def __init__(self, staff_id, listing_id, application_date):
         self.staff_id = staff_id
-        self.role_name = role_name
+        self.listing_id = listing_id
         self.application_date = application_date
         
     def json(self):
         return {
             'application_id': self.application_id,
             'staff_id': self.staff_id,
-            'role_name': self.role_name,
+            'listing_id': self.listing_id,
             'application_date': self.application_date.strftime('%Y-%m-%d')
         }
