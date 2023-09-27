58fe7c5b6945035f27953487acafc2167e142d3c
Vighnesh Ramasamy <vighneshramasamy@gmail.com>
2023-09-28 02:18:30 +0800

    add viewing individual role listing feature

--- src/components/RoleListings.vue
+++ src/components/RoleListings.vue
@@ -7,7 +7,7 @@
             Role Listings
         </div>
         <v-row class="d-flex justify-center">
-            <!-- <v-col  cols="12" md="6" lg="4"> -->
+            <!-- display role listings -->
             <v-card v-for="role in roleListings" :key="role.listing_id" class="role-card w-50 h-25">
                 <v-row class="mt-3">
                     <v-col class="d-flex justify-start">
@@ -23,41 +23,124 @@
                 </v-row>
 
                 <v-row>
-                    <v-col >
+                    <v-col>
                         <v-card-text class="d-flex justify-start text-h6">
-                            <span class="font-weight-bold">Department: </span> &nbsp <span>{{ role.department }}</span> 
+                            <span class="font-weight-bold">Department: </span> &nbsp <span>{{ role.department }}</span>
                         </v-card-text>
                         <v-card-text class="d-flex justify-start text-h6 pt-0">
-                            <span class="font-weight-bold">Deadline: </span>&nbsp<span>{{ role.deadline }}</span> 
+                            <span class="font-weight-bold">Deadline: </span>&nbsp<span>{{ role.deadline }}</span>
                         </v-card-text>
                     </v-col>
                     <v-col class="d-flex justify-end me-4 mb-4 align-end">
                         <!-- open role description page -->
-                        <v-btn class="me-3" density="default" icon="mdi-open-in-new"></v-btn>
+                        <v-btn class="me-3" density="default" icon="mdi-open-in-new" @click="openModal(role)"></v-btn>
                         <!-- edit role -->
                         <v-btn icon v-if="userIsHr"><v-icon>mdi-pencil</v-icon></v-btn>
                     </v-col>
                 </v-row>
-                <!-- You can add more card content here -->
             </v-card>
-            <!-- </v-col> -->
+
+            <v-dialog v-model="showModal" hide-overlay>
+
+                <!-- Modal content goes here -->
+                <v-card>
+                    <v-card-actions>
+                        <v-btn @click="showModal = false">Close</v-btn>
+                    </v-card-actions>
+                    <v-container>
+                        <v-row class="mt-3">
+                            <v-col class="d-flex justify-start align-center">
+                                <v-card-title class="text-h5 font-weight-bold">
+                                    {{ roleToDisplay.role_name }}
+                                </v-card-title>
+                                <v-chip color="primary">
+                            {{ roleToDisplay.category }}
+                        </v-chip>
+                            </v-col>
+                            <v-col cols="auto">
+                                <v-btn class="bg-blue-accent-4 text-h6">Apply Now</v-btn>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <v-card-text class="d-flex justify-start text-h6">
+                                    <span class="font-weight-bold">Department: </span> &nbsp <span>{{ roleToDisplay.department }}</span>
+                                </v-card-text>
+                                <v-card-text class="d-flex justify-start text-h6 pt-0">
+                                    <span class="font-weight-bold">Deadline: </span>&nbsp<span>{{ roleToDisplay.deadline }}</span>
+                                </v-card-text>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <v-card-text class="text-h6">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus magnam aspernatur culpa consequatur corrupti modi quibusdam, vitae mollitia numquam ullam doloremque fuga beatae voluptate molestias itaque dolor soluta molestiae fugit!
+                                </v-card-text>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col >
+                                <v-card-text>
+                                    <!-- loop thru requied skils -->
+                                    <span class="font-weight-bold text-h6">Skills Required: </span>
+                                    <v-chip class="me-2" v-for="skill in roleToDisplay.skills_required" :key="skill" color="blue">
+                                        {{ skill }}
+                                    </v-chip>
+                                </v-card-text>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+
+                    <!-- <v-card-title>
+                        Full-Screen Modal Content
+                    </v-card-title>
+                    <v-card-text>
+                        
+                    </v-card-text> -->
+
+                </v-card>
+            </v-dialog>
         </v-row>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
     props: {
-        roleListings: Array, // Receive role listings as props
+        roleListings: [], // Receive role listings as props
+    },
+    data() {
+        return {
+            showModal: false, // Control the visibility of the full-screen modal
+            roleToDisplay: null, // Store the role data for the modal
+        };
+    },
+    methods: {
+        openModal(role) {
+            // Store the role data and open the modal
+            // axios.get(`http://127.0.0.1:5000/hr/role_listings/${roleId}`)
+            //     .then((response) => {
+            //         console.log(response.data.data);
+            //         
+            //         console.log(this.roleToDisplay);
+            //     })
+            //     .catch((error) => {
+            //         console.error('Error fetching role listings:', error);
+            //     });
+            this.roleToDisplay = role;
+
+            this.showModal = true;
+        },
     },
 
     computed: {
         userIsHr() {
-        // Access the user's role from your Vuex store getter
-        return this.$store.getters.getUserRole === 'hr';
+            // Access the user's role from your Vuex store getter
+            return this.$store.getters.getUserRole === 'hr';
         },
-  },
+    },
 };
 </script>
 
--- src/views/RolesPage.vue
+++ src/views/RolesPage.vue
@@ -1,37 +1,61 @@
 // Inside your Vue component
 <template>
-  <div>
-    <!-- Display role listings using the RoleListings component -->
-    <RoleListings :roleListings="roleListings" />
-  </div>
+    <div>
+        <!-- Display role listings using the RoleListings component -->
+        <RoleListings :roleListings="roleListings" />
+    </div>
 </template>
 
 <script>
-import axios from 'axios';   
+import axios from 'axios';
 import RoleListings from '../components/RoleListings.vue'; // Import the RoleListings component
 
 export default {
-  data() {
-    return {
-      roleListings: [], // Initialize as an empty array
-    };
-  },
-  mounted() {
-    // Fetch role listings from the API
-    axios.get('http://127.0.0.1:5000/hr/role_listings') 
-      .then((response) => {
-        console.log(response.data.data);
-        this.roleListings = response.data.data.role_listing; // Set the retrieved data to roleListings
-      })
-      .catch((error) => {
-        console.error('Error fetching role listings:', error);
-      });
-    
-  },
-  
-  components: {
-    RoleListings, // Register the RoleListings component
-  },
+    data() {
+        return {
+            roleListings: [], // Initialize as an empty array
+        };
+    },
+    mounted() {
+        // Fetch role listings from the API
+        axios.get('http://127.0.0.1:5000/hr/role_listings')
+            .then((response) => {
+                console.log(response.data.data);
+                // this.roleListings = response.data.data.role_listing; // Set the retrieved data to roleListings
+                this.roleListings = [
+                    {
+                        "category": "Administration and Support",
+                        "deadline": "2023-09-30",
+                        "department": "HR",
+                        "listing_id": 1,
+                        "role_name": "Manager",
+                        "skills_required": [
+                            "Python",
+                            "SQL"
+                        ]
+                    },
+                    {
+                        "category": "Engineering",
+                        "deadline": "2023-10-15",
+                        "department": "IT",
+                        "listing_id": 2,
+                        "role_name": "Engineer",
+                        "skills_required": [
+                            "Python"
+                        ]
+                    }
+
+                ]
+            })
+            .catch((error) => {
+                console.error('Error fetching role listings:', error);
+            });
+
+    },
+
+    components: {
+        RoleListings, // Register the RoleListings component
+    },
 
 
 };
