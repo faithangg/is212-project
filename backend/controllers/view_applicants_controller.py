98a64fed60489c5ef74ee97f55c9e3022b08a14e
faithangg <89286947+faithangg@users.noreply.github.com>
2023-10-01 16:40:42 +0800

    hr view all applicants for a role

---
+++ backend/controllers/view_applicants_controller.py
@@ -0,0 +1,52 @@
+from flask import jsonify
+from database import db
+from models.staff import Staff
+from models.role_listing import RoleListing
+from models.job_application import JobApplication
+from blueprints.hr_blueprint import hr_blueprint
+from .role_listing_controller import role_skill_match
+
+@hr_blueprint.route('/role_applicants/<int:listing_id>', methods=['GET'])
+def view_role_applicants(listing_id):
+    try:
+        role_listing = RoleListing.query.get_or_404(listing_id)
+
+        # Fetch applicants for the role listing
+        applications = db.session.query(JobApplication, Staff).join(
+            Staff, JobApplication.staff_id == Staff.staff_id
+        ).filter(
+            JobApplication.listing_id == listing_id
+        ).all()
+        
+        if not applications:
+            return jsonify({
+                "code": 404,
+                "message": f"No applicants found for role listing {role_listing.role_name}"
+            }), 404
+
+        applicant_data = []
+
+        for application, staff in applications:
+            skill_match_data = role_skill_match(staff.staff_id, role_listing.role_name)
+            applicant_data.append({
+                "name": f"{staff.staff_fname} {staff.staff_lname}",
+                "email": staff.email,
+                "application_date": application.application_date.strftime('%Y-%m-%d'),
+                "match_percentage": skill_match_data['data']['match_percentage'] if skill_match_data['code'] == 200 else "Error"
+            })
+
+        data = {
+            "role_name": role_listing.role_name,
+            "department": role_listing.department,
+            "deadline": role_listing.deadline.strftime('%Y-%m-%d'),
+            "total_applicants": len(applicant_data),
+            "applicants": applicant_data
+        }
+
+        return jsonify({
+            "code": 200,
+            "data": data
+        }), 200
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
