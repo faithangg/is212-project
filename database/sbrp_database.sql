b9227370fb6d046a09172b36d1b67e102a99ceae
faithangg <89286947+faithangg@users.noreply.github.com>
2023-10-01 13:35:36 +0800

    improved role desc, more dummy vals, shorten passw

--- database/sbrp_database.sql
+++ database/sbrp_database.sql
@@ -9,17 +9,6 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `sbrp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `sbrp`;
 
-DROP TABLE IF EXISTS `staff_skill`;
-DROP TABLE IF EXISTS `role_skill`;
-DROP TABLE IF EXISTS `login_details`;
-DROP TABLE IF EXISTS `job_application`;
-DROP TABLE IF EXISTS `role_listing`;
-DROP TABLE IF EXISTS `role`;
-DROP TABLE IF EXISTS `category`;
-DROP TABLE IF EXISTS `staff`;   
-DROP TABLE IF EXISTS `skills`;   
-
-
 DROP TABLE IF EXISTS `access_rights`;
 CREATE TABLE IF NOT EXISTS `access_rights`(
     access_right integer NOT NULL,
@@ -46,8 +35,15 @@ CREATE TABLE IF NOT EXISTS `staff` (
 
 INSERT INTO staff (staff_id, staff_fname, staff_lname, dept, email, access_rights) VALUES
 (1, 'John', 'Doe', 'HR', 'john.doe@example.com', 2),
-(2, 'Jane', 'Smith', 'IT', 'jane.smith@example.com', 1), -- Changed to 1
-(3, 'Bob', 'Johnson', 'Finance', 'bob.johnson@example.com', 1); -- Changed to 1
+(2, 'Jane', 'Smith', 'IT', 'jane.smith@example.com', 1),
+(3, 'Bob', 'Johnson', 'Finance', 'bob.johnson@example.com', 1),
+(4, 'Alice', 'Martin', 'Sales', 'alice.martin@example.com', 2),
+(5, 'David', 'Brown', 'Legal', 'david.brown@example.com', 1),
+(6, 'Ella', 'White', 'Marketing', 'ella.white@example.com', 2),
+(7, 'Steve', 'Williams', 'IT', 'steve.williams@example.com', 2),
+(8, 'Emily', 'Jones', 'Marketing', 'emily.jones@example.com', 2),
+(9, 'Lucas', 'Miller', 'HR', 'lucas.miller@example.com', 1),
+(10, 'Lily', 'Davis', 'Finance', 'lily.davis@example.com', 1);
 
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
@@ -57,10 +53,16 @@ CREATE TABLE IF NOT EXISTS `role` (
 )  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO role (role_name, role_desc) VALUES
-('HR Manager', 'Human Resources Manager role'),
-('Software Engineer', 'Software Engineering role'),
-('Financial Analyst', 'Financial Analysis role'),
-('Network Administrator', 'Network Administration role');
+('HR Manager', 'Responsible for managing human resources operations and ensuring that HR strategies align with organizational goals.'),
+('Software Engineer', 'Develops software solutions by analyzing user requirements and designing system flow.'),
+('Financial Analyst', 'Assists in financial planning and analysis, studying financial data to assess the company’s performance.'),
+('Network Administrator', 'Manages and maintains an organization’s network, ensuring network availability to system users.'),
+('HR Assistant', 'Provides administrative support, particularly in the areas of recruitment and employee relations.'),
+('Sales Manager', 'Leads and manages sales team, sets sales goals, analyzes data, and develops training programs for sales representatives.'),
+('Marketing Specialist', 'Develops and implements marketing campaigns, tracks marketing data, and creates integrated marketing communications.'),
+('Legal Advisor', 'Provides advice on legal matters, drafts documents, ensures the legality of business practices, and provides general counsel.'),
+('System Analyst', 'Analyzes software requirements and defines program parameters and specifications.'),
+('Financial Planner', 'Advises individuals on achieving their financial goals by assessing their financial situations and developing appropriate strategies.');
 
 
 DROP TABLE IF EXISTS `skills`;
@@ -81,7 +83,17 @@ INSERT INTO skills (skill_name) VALUES
 ('Operating Systems'),
 ('Cloud Services'),
 ('Problem Solving'),
-('Troubleshooting');
+('Troubleshooting'),
+('Sales Techniques'),
+('Negotiation'),
+('Legal Analysis'),
+('Content Marketing'),
+('SEO'),
+('Java'),
+('Recruitment'),
+('Digital Marketing'), 
+('Interviewing'),
+('Budgeting');
 
 DROP TABLE IF EXISTS `role_skill`;
 CREATE TABLE IF NOT EXISTS `role_skill` (
@@ -104,7 +116,16 @@ INSERT INTO role_skill (role_name, skill_name) VALUES
 ('Network Administrator', 'Cybersecurity'),
 ('Network Administrator', 'Operating Systems'),
 ('Network Administrator', 'Cloud Services'),
-('Network Administrator', 'Troubleshooting');
+('Network Administrator', 'Troubleshooting'),
+('HR Assistant', 'Recruitment'),
+('Sales Manager', 'Sales Techniques'),
+('Sales Manager', 'Negotiation'),
+('Marketing Specialist', 'Content Marketing'),
+('Marketing Specialist', 'SEO'),
+('Legal Advisor', 'Legal Analysis'),
+('System Analyst', 'Java'),
+('System Analyst', 'SQL'),
+('Financial Planner', 'Budgeting');
 
 DROP TABLE IF EXISTS `staff_skill`;
 CREATE TABLE IF NOT EXISTS `staff_skill` (
@@ -116,9 +137,16 @@ CREATE TABLE IF NOT EXISTS `staff_skill` (
 )  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO staff_skill (staff_id, skill_name) VALUES
-(1, 'Python'),
-(2, 'SQL'),
-(3, 'Finance');
+(1, 'Recruitment'), (1, 'Problem Solving'), (1, 'Excel'), (1, 'SQL'), (1, 'Sales Techniques'), (1, 'Networking'),
+(2, 'Python'), (2, 'SQL'), (2, 'Legal Analysis'), (2, 'Sales Techniques'), (2, 'Budgeting'),
+(3, 'Finance'), (3, 'Excel'), (3, 'SQL'), (3, 'Sales Techniques'), (3, 'Networking'), (3, 'Legal Analysis'),
+(4, 'Sales Techniques'), (4, 'Negotiation'), (4, 'Excel'), (4, 'SEO'), (4, 'Finance'), (4, 'Networking'),
+(5, 'Legal Analysis'), (5, 'Problem Solving'), (5, 'SQL'), (5, 'Finance'), (5, 'Content Marketing'),
+(6, 'Content Marketing'), (6, 'SEO'), (6, 'Java'), (6, 'Sales Techniques'), (6, 'Excel'),
+(7, 'Java'), (7, 'Python'), (7, 'Problem Solving'), (7, 'Legal Analysis'), (7, 'Sales Techniques'), (7, 'Finance'),
+(8, 'SEO'), (8, 'Digital Marketing'), (8, 'SQL'), (8, 'Java'), (8, 'Sales Techniques'), (8, 'Legal Analysis'),
+(9, 'Recruitment'), (9, 'Interviewing'), (9, 'Networking'), (9, 'Java'), (9, 'Sales Techniques'), (9, 'Finance'),
+(10, 'Budgeting'), (10, 'Excel'), (10, 'SQL'), (10, 'Java'), (10, 'Legal Analysis'), (10, 'Networking');
 
 
 DROP TABLE IF EXISTS `category`;
@@ -136,7 +164,7 @@ INSERT INTO category (category, category_desc) VALUES
 ('Design and Technical Service', 'Involves developing creative, tech-based solutions to existing problems'),
 ('Information Technology', 'Involves the implementation, support, maintenance, repair or protection of data or computer systems'),
 ('Engineering', 'Jobs that use science and mathematics to solve a variety of problems'),
-('Finance', "Kobs where a company's funds and profitability are the primary focus"),
+('Finance', "Jobs where a company's funds and profitability are the primary focus"),
 ('Legal', 'Practicing the law'),
 ('Manufacturing and production', 'Involves the creation of new products either from raw materials or by assembling different components'),
 ('Marketing and advertising', "Responsible for planning, creating, and executing marketing campaigns to expand their company's reach and potential customer"),
@@ -155,10 +183,17 @@ CREATE TABLE IF NOT EXISTS `role_listing` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO role_listing (listing_id, role_name, department, category, deadline) VALUES
-(1, 'HR Manager', 'HR', 'Administration and Support', '2023-09-30'),
-(2, 'Software Engineer', 'IT', 'Engineering', '2023-10-15'),
-(3, 'Financial Analyst', 'Finance', 'Finance', '2023-08-15'),
-(4, 'Network Administrator', 'IT', 'Information Technology', '2024-08-15');
+(1, 'HR Manager', 'HR', 'Administration and Support', '2023-09-25'), 
+(2, 'Software Engineer', 'IT', 'Engineering', '2023-10-10'), 
+(3, 'Financial Analyst', 'Finance', 'Finance', '2023-10-10'), 
+(4, 'Network Administrator', 'IT', 'Information Technology', '2024-12-15'), 
+(5, 'HR Assistant', 'HR', 'Administration and Support', '2023-11-10'),
+(6, 'Sales Manager', 'Sales', 'Sales and Communication', '2024-11-30'), 
+(7, 'Marketing Specialist', 'Marketing', 'Marketing and advertising', '2023-11-20'), 
+(8, 'Legal Advisor', 'Legal', 'Legal', '2024-12-20'), 
+(9, 'System Analyst', 'IT', 'Information Technology', '2024-10-10'), 
+(10, 'Financial Planner', 'Finance', 'Finance', '2023-10-05'); 
+
 
 DROP TABLE IF EXISTS `job_application`;
 CREATE TABLE IF NOT EXISTS `job_application` (
@@ -172,9 +207,16 @@ CREATE TABLE IF NOT EXISTS `job_application` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO job_application (application_id, staff_id, listing_id, application_date) VALUES
-(1, 1, 1, '2023-09-20'),
-(2, 2, 2, '2023-10-10');
-
+(1, 1, 1, '2023-09-19'), 
+(2, 2, 2, '2023-10-01'), 
+(3, 3, 3, '2023-08-01'), 
+(4, 4, 5, '2023-09-05'), 
+(5, 5, 7, '2023-09-15'), 
+(6, 6, 6, '2023-07-10'), 
+(7, 7, 9, '2023-09-01'), 
+(8, 8, 4, '2023-07-25'), 
+(9, 9, 10, '2023-09-25'), 
+(10, 10, 8, '2023-09-20'); 
 
 DROP TABLE IF EXISTS `login_details`;
 CREATE TABLE IF NOT EXISTS `login_details` (
@@ -185,17 +227,15 @@ CREATE TABLE IF NOT EXISTS `login_details` (
 )  ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO login_details (staff_id, staff_password) VALUES
-(1, 'hashed_password_for_john'),
-(2, 'hashed_password_for_jane'),
-(3, 'hashed_password_for_bob');
+(1, 'john'),
+(2, 'jane'),
+(3, 'bob'),
+(4, 'alice'),
+(5, 'david'),
+(6, 'ella'),
+(7, 'steve'),
+(8, 'emily'),
+(9, 'lucas'),
+(10, 'lily');
 
-
-DROP TABLE IF EXISTS `extra`;
-CREATE TABLE IF NOT EXISTS `extra` (
-    `extra_id` int(11) NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`extra_id`)
-)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-INSERT INTO `extra` (`extra_id`)VALUES 
-('1');
-
+COMMIT;
