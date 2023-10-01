7c23f029bdadd4785ec659b7f466ab5dd81e4bcc
faithangg <89286947+faithangg@users.noreply.github.com>
2023-10-01 16:13:38 +0800

    role skill match more concise

--- backend/controllers/role_listing_controller.py
+++ backend/controllers/role_listing_controller.py
@@ -43,48 +43,33 @@ def filter_listings_by_staff_and_deadline(query, staff_id):
 def role_skill_match(staff_id, role_name):
     try:
         # Get all the skills based on the roles
-        role_skills_db = RoleSkill.query.filter_by(role_name=role_name).all()
+        role_skills = [rs.skill_name for rs in RoleSkill.query.filter_by(role_name=role_name).all()]
 
         # Get all the skills that the staff has
-        staff_skills_db = StaffSkill.query.filter_by(staff_id=staff_id).all()
+        staff_skills = [ss.skill_name for ss in StaffSkill.query.filter_by(staff_id=staff_id).all()]
 
-        # Store all the staff skills in an array
-        staff_skills = []
+        # Determine skills that staff have and don't have based on the role's requirements
+        staff_have = [skill for skill in role_skills if skill in staff_skills]
+        staff_dont = [skill for skill in role_skills if skill not in staff_skills]
 
-        for skill in staff_skills_db:
-            staff_skills.append(skill.skill_name)
-
-
-        # Loop through and check which skills that staff and lack
-        staff_have = []
-        staff_dont = []
-
-        for role_skill in role_skills_db:
-            current = role_skill.skill_name
-            if current in staff_skills:
-                staff_have.append(current)
-            else:
-                staff_dont.append(current)
-
-        # Get the percentage of roles matched -- ???
-        match_percentage = (len(staff_have) / (len(staff_have) + len(staff_dont))) * 100
-
-        match_percentage = str(match_percentage)
+        # Calculate the percentage of roles matched
+        match_percentage = (len(staff_have) / len(role_skills)) * 100
 
         return {
-                    "code": 200,
-                    "data": {
-                        "have": [have for have in staff_have],
-                        "dont": [dont for dont in staff_dont],
-                        "match_percentage": match_percentage
-                    }
-                }
+            "code": 200,
+            "data": {
+                "have": staff_have,
+                "dont": staff_dont,
+                "match_percentage": str(match_percentage)
+            }
+        }
     except Exception as e:
         # Return an error in a dictionary format
         return {
             "code": 500,
             "error": str(e)
         }
+
 ####################################################################################################################################
 
 # HR: GET ALL ROLE LISTINGS
