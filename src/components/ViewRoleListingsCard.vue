df56c9cfe31dd028449672740e6db882f602a06d
Xinyi Tan <xinyi.tan.2021@scis.smu.edu.sg>
2023-10-04 11:57:10 +0800

    Updating of filter ui
    moving code to the viewRolesPage.vue file

--- src/components/ViewRoleListingsCard.vue
+++ src/components/ViewRoleListingsCard.vue
@@ -3,117 +3,8 @@
 <template>
   <div>
     <v-row class="d-flex justify-center">
-            <!-- display filter listings -->
-        <v-col :cols="2" class="justify-start">
-            <div class="text-h5 text-left">
-              Filters
-            </div>
-       
-
-          <v-col cols="12" sm="9" md="4" class="text-left">
-            <v-btn @click="clearFilter" flat text size="small">Clear All</v-btn>
-          </v-col>
-          <v-expansion-panels
-            multiple>
-            <!--Category Filter-->
-            <v-expansion-panel>
-              <v-expansion-panel-title>Category</v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <v-container fluid>
-                  <v-checkbox
-                    v-for="(item, index) in categoryItems"
-                    :key="index"
-                    v-model="selectedItems"
-                    :label="item"
-                    :value="item"
-                  ></v-checkbox>
-
-                </v-container>
-              </v-expansion-panel-text>  
-            </v-expansion-panel>
-            <!--Department Filter -->
-            <v-expansion-panel>
-              <v-expansion-panel-title>Department</v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <v-container fluid>
-                  <v-checkbox
-                    v-for="(item, index) in departmentItems"
-                    :key="index"
-                    v-model="selectedItems"
-                    :label="item"
-                    :value="item"
-                  ></v-checkbox>
-
-                </v-container>
-              </v-expansion-panel-text>  
-            </v-expansion-panel>
-            <!--Match Percentage Filter hardcoded-->
-            <v-expansion-panel>
-              <v-expansion-panel-title>Match Percentage </v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <v-container fluid>
-                  <p>{{ selected }}</p>
-                  <v-checkbox
-                    v-model="selected"
-                    label="0 - 10"
-                    value="0 - 10"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="11-20"
-                    value="11-20"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="21 - 30"
-                    value="21 - 30"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="31 - 40"
-                    value="31 - 40"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="41 - 50"
-                    value="41 - 50"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="51 - 60"
-                    value="51 - 60"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="61 - 70"
-                    value="61 - 70"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="71 - 80"
-                    value="71 - 80"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="81 - 90"
-                    value="81 - 90"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="selected"
-                    label="91 - 100"
-                    value="91 - 100"
-                  ></v-checkbox>
-                </v-container>
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-          <v-col cols="12" sm="9" md="4">
-            <v-btn @click="applyFilter" density="default">Apply Filter</v-btn>
-          </v-col>
-        </v-col>
-        
+ 
       <!-- display role listings -->
-      <v-col :cols="8" class="justify-end">
 
       <v-card
         v-for="role in displayListings"
@@ -171,7 +62,7 @@
           </v-col>
         </v-row>
       </v-card>
-    </v-col>
+  
 
       <v-dialog v-model="showModal" hide-overlay>
         <!-- Modal content goes here -->
@@ -311,23 +202,11 @@ export default {
       success_model: false, // Control the visibility of the full-screen success modal
       failure_model: false, // Control the visibility of the full-screen failure modal
       appliedRole: null,
-      items: [],
-      selectedItems: [],
+
     };
   },
   mounted(){
-    // Fetch filter options from the API
-    axios.get(`http://127.0.0.1:5000/staff/filter_options`)
-        .then((response) => {
-            console.log(response.data.data);
-            console.log(response.data.data.category);
 
-            this.categoryItems = response.data.data.category;
-            this.departmentItems = response.data.data.department;
-        })
-        .catch((error) => {
-            console.error('Error fetching filter options:', error);
-        });
 
   },
 
@@ -389,12 +268,7 @@ export default {
           }, 3000);
         });
     },
-    applyFilter(){
-      // Fetch results
-    },
-    clearFilter(){
-      // Clear all filters
-    }
+
   },
 
 };
@@ -407,9 +281,5 @@ export default {
   padding: 16px;
   margin: 16px;
 }
-.custom-checkbox {
-  margin: 1px 0; /* Adjust this value to control vertical spacing */
-  padding: 1px; /* Adjust this value to control horizontal spacing */
 
-}
 </style>
--- src/views/ViewRolesPage.vue
+++ src/views/ViewRolesPage.vue
@@ -44,10 +44,122 @@
             </v-row>
 
             <!-- Search button -->
+        
         </v-container>
+        <!-- display filter listings -->
+        <v-row class="d-flex justify-center">
 
+           <v-col :cols="2" class="justify-start">
+            <div class="text-h5 text-left">
+              Filters
+            </div>
+
+          <v-col cols="12" sm="9" md="4" class="text-left">
+            <v-btn @click="clearFilter" flat text size="small">Clear All</v-btn>
+          </v-col>
+          <v-expansion-panels
+            multiple>
+            <!--Category Filter-->
+            <v-expansion-panel>
+              <v-expansion-panel-title>Category</v-expansion-panel-title>
+              <v-expansion-panel-text>
+                <v-container fluid>
+                  <v-checkbox
+                    v-for="(item, index) in categoryItems"
+                    :key="index"
+                    v-model="selectedItems"
+                    :label="item"
+                    :value="item"
+                  ></v-checkbox>
+
+                </v-container>
+              </v-expansion-panel-text>  
+            </v-expansion-panel>
+            <!--Department Filter -->
+            <v-expansion-panel>
+              <v-expansion-panel-title>Department</v-expansion-panel-title>
+              <v-expansion-panel-text>
+                <v-container fluid>
+                  <v-checkbox
+                    v-for="(item, index) in departmentItems"
+                    :key="index"
+                    v-model="selectedItems"
+                    :label="item"
+                    :value="item"
+                  ></v-checkbox>
+
+                </v-container>
+              </v-expansion-panel-text>  
+            </v-expansion-panel>
+            <!--Match Percentage Filter hardcoded-->
+            <v-expansion-panel>
+              <v-expansion-panel-title>Match Percentage </v-expansion-panel-title>
+              <v-expansion-panel-text>
+                <v-container fluid>
+                  <p>{{ selected }}</p>
+                  <v-checkbox
+                    v-model="selected"
+                    label="0 - 10"
+                    value="0 - 10"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="11-20"
+                    value="11-20"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="21 - 30"
+                    value="21 - 30"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="31 - 40"
+                    value="31 - 40"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="41 - 50"
+                    value="41 - 50"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="51 - 60"
+                    value="51 - 60"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="61 - 70"
+                    value="61 - 70"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="71 - 80"
+                    value="71 - 80"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="81 - 90"
+                    value="81 - 90"
+                  ></v-checkbox>
+                  <v-checkbox
+                    v-model="selected"
+                    label="91 - 100"
+                    value="91 - 100"
+                  ></v-checkbox>
+                </v-container>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+          </v-expansion-panels>
+          <v-col cols="12" sm="9" md="4">
+            <v-btn @click="applyFilter" density="default">Apply Filter</v-btn>
+          </v-col>
+        </v-col>
+        <!-- display role listings -->
+        <v-col :cols="8" class="justify-end">
         <ViewRoleListingsCard :role_listings_with_skill_match="displayListings" />
-
+        </v-col>
+        </v-row>
     </div>
 </template>
 
@@ -63,6 +175,8 @@ export default {
             searchQuery: '', // Initialize as an empty string
             searchQueryError: null,  
             searchQueryErrorMsg:  '', // Initialize as an empty string
+            items: [],
+            selectedItems: [],
         };
     },
     mounted() {
@@ -81,6 +195,19 @@ export default {
             })
             .catch((error) => {
                 console.error('Error fetching role listings:', error);
+            });
+        
+            // Fetch filter options from the API
+        axios.get(`http://127.0.0.1:5000/staff/filter_options`)
+            .then((response) => {
+                console.log(response.data.data);
+                console.log(response.data.data.category);
+
+                this.categoryItems = response.data.data.category;
+                this.departmentItems = response.data.data.department;
+            })
+            .catch((error) => {
+                console.error('Error fetching filter options:', error);
             });
 
     },
@@ -132,6 +259,12 @@ export default {
                 }
             });
         },
+        applyFilter(){
+        // Fetch results
+        },
+        clearFilter(){
+        // Clear all filters
+        }
     },
 
     components: {
