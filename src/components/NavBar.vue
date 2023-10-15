f0243c7d0c5af4f28146887c38e4eb00d081e7e5
Balveen Kaur <balveenkaur01@gmail.com>
2023-10-15 11:20:35 +0800

    mobile responsive navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <v-layout>
     <v-app-bar
+      app
       color="teal-darken-4"
       image="https://imageio.forbes.com/specials-images/imageserve/60ee028005bb1a95e8118143/0x0.jpg?format=jpg&crop=2915,1640,x0,y124,safe&width=1200"
     >
@@ -10,64 +11,113 @@
         ></v-img>
       </template>
 
-      <v-app-bar-title style="text-align: left;"> <img v-bind:src="require('../assets/sbrp.png')" style="width: 200px; margin-top: 20px;"/></v-app-bar-title>
-      
+      <v-app-bar-nav-icon
+        @click="toggleDrawer"
+        class="d-lg-none d-flex"
+        v-if="isLoggedIn"
+      >
+      </v-app-bar-nav-icon>
+      <v-app-bar-title style="text-align: left">
+        <img
+          v-bind:src="require('../assets/sbrp.png')"
+          style="width: 200px; margin-top: 20px"
+      /></v-app-bar-title>
+
       <v-spacer></v-spacer>
 
-      <v-btn v-if="isLoggedIn" to="/viewRolesPage" id="role_listings">View Role Listings</v-btn>
+      <v-btn
+        v-if="isLoggedIn"
+        to="/viewRolesPage"
+        id="role_listings"
+        class="d-none d-lg-block pt-2"
+        >View Role Listings</v-btn
+      >
 
-      <v-btn v-if="userIsHr" to="/manageRolesPage" id="managed">Manage Role Listings</v-btn>
-      
-      <v-btn v-if="isLoggedIn" to="/profile" id="profile">Profile</v-btn>
-      
+      <v-btn
+        v-if="userIsHr"
+        to="/manageRolesPage"
+        id="managed"
+        class="d-none d-lg-block pt-2"
+        >Manage Role Listings</v-btn
+      >
+
+      <v-btn
+        v-if="isLoggedIn"
+        to="/profile"
+        id="profile"
+        class="d-none d-lg-block pt-2"
+        >Profile</v-btn
+      >
+
       <!-- <v-btn v-if="!isLoggedIn" to="/login" id="login">Login</v-btn> -->
-
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-btn
+          v-if="isLoggedIn"
+          to="/viewRolesPage"
+          id="role_listings"
+          class="d-none d-lg-block pt-2"
+          >View Role Listings</v-btn
+        >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-btn to="/viewRolesPage">View Role Listings</v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn to="/manageRolesPage" v-if="userIsHr">Manage Role Listings</v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn to="/profile">Profile</v-btn>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-layout>
 </template>
 
 <script>
-
 export default {
-name: 'NavBar',
-mounted() {
-
-
-},
+  name: "NavBar",
+  mounted() {},
 
   data() {
     return {
-      
-    }
-},
-computed: {
-      isLoggedIn() {
-          // Access the user's login status from your Vuex store getter
-          return this.$store.getters.getIsLoggedIn;
-      },
-      userIsHr() {
-          // Access the user's role from your Vuex store getter
-          return this.$store.getters.getUserRole === 'hr';
-      },
-},
+      drawer: false,
+    };
+  },
+  computed: {
+    isLoggedIn() {
+      // Access the user's login status from your Vuex store getter
+      return this.$store.getters.getIsLoggedIn;
+    },
+    userIsHr() {
+      // Access the user's role from your Vuex store getter
+      return this.$store.getters.getUserRole === "hr";
+    },
+  },
 
-methods: {
-
-}
-
-
-}
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+  },
+};
 </script>
 
-<style >
+<style>
 .navbar {
-position: absolute;
-top: 0;
-width: 100vw;
+  position: absolute;
+  top: 0;
+  width: 100vw;
 }
 
 .link-style {
-text-decoration: none;
-color: white;
+  text-decoration: none;
+  color: white;
 }
 </style>
