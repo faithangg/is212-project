669d8b864d460b22754bf69c1d9b648a35478a83
Xinyi Tan <xinyi.tan.2021@scis.smu.edu.sg>
2023-10-15 16:27:01 +0800

    Filter component for role applicants

--- src/views/RoleApplicantsPage.vue
+++ src/views/RoleApplicantsPage.vue
@@ -15,17 +15,6 @@
                     </v-row>
                 </v-col>
             </v-row>
-            <!-- Search input field -->
-            <v-row class="d-flex justify-center mt-3 mb-6 ">
-                <v-col cols="7" class="pe-0">
-                    <v-text-field v-model="searchQuery" label="Search by ??" outlined dense hide-details
-                        class="rounded-top-left rounded-bottom-left"></v-text-field>
-                </v-col>
-                <v-col cols="1" class="d-flex justify-start ms-0 ps-0">
-                    <!-- Search button attached to the end of the search bar -->
-                    <v-btn @click="performSearch" color="teal-lighten-3" class="ma-0" style="height: 100%;">Search</v-btn>
-                </v-col>
-            </v-row>
         </v-container>
         <v-container>
             <v-row class="mt-1">
@@ -63,63 +52,12 @@
               </v-btn>
             </v-toolbar>
             <!--Filter component 1-->
-            <v-card-text>
-  <!-- Filter section content here -->
-  <!-- ... -->
-  <v-card class="mx-auto">
-
-
-    <v-card-text>
-      <h2 class="text-h6 mb-2">
-        Category
-      </h2>
-
-      <v-chip-group v-model="selectedCategory" column multiple>
-        <v-chip class="me-2" filter variant="outlined" v-for="category in categoryItems" :key="category"
-          :value="category" :id="'category_' + category">
-          {{ category }}
-
-        </v-chip>
-
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-text>
-      <h2 class="text-h6 mb-2">
-        Department
-      </h2>
-
-      <v-chip-group v-model="selectedDepartment" column multiple>
-        <v-chip class="me-2" filter variant="outlined" v-for="department in departmentItems" :key="department"
-          :value="department" :id="'department_' + department">
-          {{ department }}
-
-        </v-chip>
-
-      </v-chip-group>
-    </v-card-text>
-    
-    <v-card-text>
-      <h2 class="text-h6 mb-2">
-        Match Percentage(%)
-      </h2>
-
-      <v-chip-group v-model="selectedPercentage" column multiple>
-        <v-chip class="me-2" filter variant="outlined" v-for="percentageMatch in percentageMatchItems"
-          :key="percentageMatch" :value="percentageMatch" :id="percentageMatch">
-          {{ percentageMatch }}
-
-        </v-chip>
-
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-actions>
-              <v-btn @click="applyFilter();hideFilterModal()">Apply Filter</v-btn>
-              <v-btn @click="clearFilter();hideFilterModal()" color="error">Clear All</v-btn>
-            </v-card-actions>
-  </v-card>
-            </v-card-text>
+            <ApplicantFilterCard
+              :departmentItems="departmentItems"
+              :percentageMatchItems="percentageMatchItems"
+              @filter-applied="handleFilterApplied"
+              @filter-cleared="handleFilterCleared"
+            ></ApplicantFilterCard>
           </v-card>
         </v-dialog>
       </v-col>
@@ -127,65 +65,14 @@
 
       <v-col cols="3" class="d-none d-lg-block ms-2">
             <!--Filter component 2-->
-            <v-card-text>
-            <!-- Filter section content here -->
-            <!-- ... -->
-            <v-card class="mx-auto">
-
-
-                <v-card-text>
-                <h2 class="text-h6 mb-2">
-                    Category
-                </h2>
-
-                <v-chip-group v-model="selectedCategory" column multiple>
-                    <v-chip class="me-2" filter variant="outlined" v-for="category in categoryItems" :key="category"
-                    :value="category" :id="'category_' + category">
-                    {{ category }}
-
-                    </v-chip>
-
-                </v-chip-group>
-                </v-card-text>
-
-                <v-card-text>
-                <h2 class="text-h6 mb-2">
-                    Department
-                </h2>
-
-                <v-chip-group v-model="selectedDepartment" column multiple>
-                    <v-chip class="me-2" filter variant="outlined" v-for="department in departmentItems" :key="department"
-                    :value="department" :id="'department_' + department">
-                    {{ department }}
-
-                    </v-chip>
-
-                </v-chip-group>
-                </v-card-text>
-                
-                <v-card-text>
-                <h2 class="text-h6 mb-2">
-                    Match Percentage(%)
-                </h2>
-
-                <v-chip-group v-model="selectedPercentage" column multiple>
-                    <v-chip class="me-2" filter variant="outlined" v-for="percentageMatch in percentageMatchItems"
-                    :key="percentageMatch" :value="percentageMatch" :id="percentageMatch">
-                    {{ percentageMatch }}
-
-                    </v-chip>
-
-                </v-chip-group>
-                </v-card-text>
-
-                <v-card-actions>
-                        <v-btn @click="applyFilter();hideFilterModal()">Apply Filter</v-btn>
-                        <v-btn @click="clearFilter();hideFilterModal()" color="error">Clear All</v-btn>
-                        </v-card-actions>
-            </v-card>
-            </v-card-text>
+            <ApplicantFilterCard
+              :departmentItems="departmentItems"
+              :percentageMatchItems="percentageMatchItems"
+              @filter-applied="handleFilterApplied"
+              @filter-cleared="handleFilterCleared"
+              ></ApplicantFilterCard> 
       </v-col>
-      <!-- display role listings -->
+      <!-- display applicants -->
       <v-col :cols="9" lg="" class="justify-end" id="filter_alert">
         <v-alert v-if="filterError == 404" type="info" variant="outlined" icon="$info"
           style="font-size: 16px; padding: 8px; height: auto;" dismissible>
@@ -202,10 +89,13 @@
 <script>
 import axios from 'axios';
 import RoleApplicantsCard from '../components/RoleApplicantsCard.vue'; // Import the RoleApplicantsCard component
+import ApplicantFilterCard from '../components/ApplicantFilterCard.vue'; // Import the ApplicantFilterCardCard component
+
 
 export default {
     data() {
         return {
+            applicantsFromDb:[],
             listing_id: null, // Initialize as an empty array
             role_applicants: [],
             deadline:null ,
@@ -213,6 +103,14 @@ export default {
             role_name: null,
             total_applicants: null,
             searchQuery: '', // Initialize as an empty string
+            showFilterModal: false, // Initialize as false to hide the modal initially
+            filterError: null,
+            filterErrorMsg: '',
+            categoryItems: [],
+            departmentItems: [],
+            selectedDepartment: [],
+            selectedPercentage: [],
+            showFilterModal: false, // Initialize as false to hide the modal initially
         };
     },
     mounted() {
@@ -221,17 +119,16 @@ export default {
         axios.get(`http://127.0.0.1:5000/hr/role_applicants/${this.listing_id}`)
             .then((response) => {
                 console.log(response.data.data);
-
-                //this.rolesFromDb = response.data.data.role_listings_with_skill_match;
-                this.role_applicants = response.data.data.applicants;
+                this.applicantsFromDb = response.data.data.applicants;
                 console.log(this.role_applicants[0]);
+                this.role_applicants=this.applicantsFromDb
                 this.deadline = response.data.data.deadline,
                 this.department = response.data.data.department,
                 this.role_name = response.data.data.role_name,
                 this.total_applicants = response.data.data.total_applicants;
             })
             .catch((error) => {
-                console.error('Error fetching role listings:', error);
+                console.error('Error fetching applicants:', error);
             });
 
     },
@@ -240,31 +137,80 @@ export default {
     },
 
     methods: {
-        performSearch() {
-            // This method is called when the Search button is clicked.
-           
-            if (this.searchQuery == '') {
-                this.displayListings = this.rolesFromDb;
-                return;
-            }
-            // Fetch role listings from the API
-            var userId = this.getUserId;
+    applyFilter() {
+      // Fetch results
+      this.filterError = '';
+      this.filterErrorMsg = '';
 
-            axios.get(`http://127.0.0.1:5000/staff/browse_role_listings/${userId}/${this.searchQuery}`)
-            .then((response) => {
-                console.log(response.data.data);
+      if (this.selectedDepartment.length == 0 && this.selectedPercentage.length == 0) {
+        this.role_applicants = this.rolesFromDb;
+        return;
+      }
 
-                this.displayListings = response.data.data.role_listings_with_skill_match;
-            })
-            .catch((error) => {
-                console.error('Error fetching role applicants:', error);
-            });
-        },
+      console.log(this.selectedDepartment);
+      console.log(this.selectedPercentage);
+
+      let payload = {
+        department: Array.from(this.selectedDepartment),
+        match_percentage: Array.from(this.selectedPercentage),
+      };
+
+
+      axios.post(`http://127.0.0.1:5000/hr/filter_applicants/${this.listing_id}`, payload)
+        .then((response) => {
+          console.log(response.data.data);
+          this.role_applicants = response.data.data.applicants;
+
+
+        })
+        .catch((error) => {
+          console.error('Error fetching applicants:', error);
+          this.filterErrorMsg = 'No applicants found based on your input filters';
+          this.filterError = 404;
+          this.role_applicants = [];
+
+        });
+
+
+
+    },
+    handleFilterApplied(selectedOptions) {
+      // Handle the applied filter here
+      this.selectedDepartment = selectedOptions.selectedDepartment;
+      this.selectedPercentage = selectedOptions.selectedPercentage;
+
+      this.applyFilter(); // Call the method to apply the filter with the updated options
+    },
+
+    handleFilterCleared() {
+      // Handle the cleared filter here
+      this.selectedDepartment = [];
+      this.selectedPercentage = [];      
+      this.clearFilter(); // Call the method to clear the filter with the updated options
+    },
+
+    clearFilter() {
+      // Clear all filters
+      this.selectedDepartment = [];
+      this.selectedPercentage = [];
+      this.role_applicants = this.applicantsFromDb;
+      this.filterError = '';
+      this.filterErrorMsg = '';
+    },
+    showFilter() {
+      this.showFilterModal = true; // Show the filter modal
+    },
+
+    hideFilterModal() {
+      this.showFilterModal = false; // Hide the filter modal
+    },
+
     },
 
     components: {
          // Register the RoleListings component
          RoleApplicantsCard,
+         ApplicantFilterCard,
     },
 
 
