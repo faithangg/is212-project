53af35bcca7ed89c208e6635251229894bf786d9
Balveen Kaur <balveenkaur01@gmail.com>
2023-10-21 20:20:09 +0800

    added comments

--- test_files/selenium_tests/hr/sort_role_applicants.py
+++ test_files/selenium_tests/hr/sort_role_applicants.py
@@ -7,33 +7,42 @@ import unittest
 
 driver = webdriver.Chrome()
 driver.maximize_window()
+
+# Login first
 driver.get("http://localhost:8080")
+
+# Enter staff id
 staff_id = driver.find_element(By.ID, "staffId")
 staff_id.clear()
 staff_id.send_keys(160332)
 
+# Enter password
 password = driver.find_element(By.ID, "password")
 password.clear()
 password.send_keys("somchai")
 
+# Click login button
 driver.find_element(By.ID, "login").click()
 
 time.sleep(3)
 
+# Go to managed role listing page
 text = driver.find_element(By.ID, "managed").click()
-
 time.sleep(3)
 
+# Open the first modal
 element = driver.find_element(By.ID, "view_applicants")
 element.click()
 time.sleep(3)
 
 try:
+    # Sort applicants by match percentage
     sort = driver.find_element(By.ID, "sort_dropdown")
     sort.send_keys(Keys.RETURN)
     time.sleep(1)
     driver.find_element(By.XPATH, '//*[text() = "Match Percentage (High to Low)"]').click()
     time.sleep(1)
+    # Check if the email is displayed
     text = driver.find_element(By.ID, "email").text
     print(text)
     assert "Email:" in text
