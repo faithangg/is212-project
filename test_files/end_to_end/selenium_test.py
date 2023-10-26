304f3237ec2c28272be33db22d0c9dbe113dffc1
Balveen Kaur <balveenkaur01@gmail.com>
2023-10-26 21:40:50 +0800

    changes yml file

--- test_files/end_to_end/selenium_test.py
+++ test_files/end_to_end/selenium_test.py
@@ -3,6 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import unittest
+import xmlrunner
 
 class Staff(unittest.TestCase):
     @classmethod
@@ -532,4 +533,4 @@ class Staff(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
